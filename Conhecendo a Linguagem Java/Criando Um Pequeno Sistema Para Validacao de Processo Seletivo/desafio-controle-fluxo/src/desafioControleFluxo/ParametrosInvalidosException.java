2bf4d1a9d009df18a93994083c6ec6bdac593e8a
pedroBaptistaDosSantos <pedrobaptista.dev@gmail.com>
2023-09-08 13:50:03 -0300

    Exercicio

---
+++ Conhecendo a Linguagem Java/Criando Um Pequeno Sistema Para Validacao de Processo Seletivo/desafio-controle-fluxo/src/desafioControleFluxo/ParametrosInvalidosException.java
@@ -0,0 +1,14 @@
+package desafioControleFluxo;
+
+import java.util.Scanner;
+
+public class ParametrosInvalidosException {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Digite o primeiro valor:")
+		int valor1 = scan.nextInt();
+		System.out.println("Digite o segundo valor:")
+		int valor2 = scan.nextInt();
+	}
+}
