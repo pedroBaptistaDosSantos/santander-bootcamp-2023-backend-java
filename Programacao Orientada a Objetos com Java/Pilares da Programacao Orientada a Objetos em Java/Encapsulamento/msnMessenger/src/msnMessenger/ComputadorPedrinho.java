f016f3b4922413987a6f182a854281c49399fa84
pedroBaptistaDosSantos <pedrobaptista.dev@gmail.com>
2023-09-27 17:23:24 -0300

    Encapsulamento

---
+++ Programacao Orientada a Objetos com Java/Pilares da Programacao Orientada a Objetos em Java/Encapsulamento/msnMessenger/src/msnMessenger/ComputadorPedrinho.java
@@ -0,0 +1,12 @@
+package msnMessenger;
+
+public class ComputadorPedrinho {
+	public static void main(String[] args) {
+		//abrindo MSN Messenger
+		MSNMessenger msn = new MSNMessenger();
+
+		msn.enviarMensagem();
+
+		msn.receberMensagem();
+	}
+}
